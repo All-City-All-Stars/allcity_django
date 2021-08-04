52cae6ea4e00d4c5e5b3ca4fa71d4250c5f6816c
yetifunk <charlesdemar87@gmail.com>
2021-08-04 08:04:32 -0600

    hide secret key

--- allcity_django/settings.py
+++ allcity_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&u2ucl8#ex!qpwe@3yqqy*npcwho%&mhjpaq0ht8x%rmf$m%=k'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
